0f4efef206703a8de96ee02d56336d90183124fe
mustafarukesen <mustafarukesen@gmail.com>
2022-11-29 22:01:18 +0300

    first commit

---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mesen <mesen@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 20:49:19 by mesen             #+#    #+#             */
+/*   Updated: 2022/10/27 20:49:19 by mesen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int		ft_putchar(int c);
+int		ft_putstr(char *var);
+int		ft_printf(const char *var, ...);
+
+#endif
